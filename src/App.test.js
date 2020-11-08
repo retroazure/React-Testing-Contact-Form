6091ad952c722f192ab98d6f28c076cb5bcdc16d
retroazure <tomascosta005@outlook.com>
2020-11-08 21:51:26 +0000

    First wave of testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import * as rt from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  rt.render(<App />);
+ })
+
+ test("Form submit displays its values", () => {
+   
+  rt.render(<App />);
+
+   const firstName = rt.screen.getByLabelText(/First Name/i);
+   const lastName = rt.screen.getByLabelText(/Last Name/i);
+   const email = rt.screen.getByLabelText(/Email/i);
+   const message = rt.screen.getByLabelText(/Message/i);
+   const submitButton = rt.screen.getByText(/submit/i);
+
+   rt.act(()=> {
+     rt.fireEvent.change(firstName, { target: { value: 'Ana' }});
+     rt.fireEvent.change(lastName, { target: { value: 'Maria' }});
+     rt.fireEvent.change(email, { target: { value: 'something@gmail.com' }});
+     rt.fireEvent.change(message, { target: { value: 'Hello World' }});
+
+ });
+
+ rt.fireEvent.click(submitButton);
+
 });
